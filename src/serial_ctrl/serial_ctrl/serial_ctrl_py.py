b55c318226820ea39f7269fa716eb8bceada2491
Haolun <mihawkhhuang@gmail.com>
2025-01-21 22:34:03 +0800

    just a test comment

--- src/serial_ctrl/serial_ctrl/serial_ctrl_py.py
+++ src/serial_ctrl/serial_ctrl/serial_ctrl_py.py
@@ -8,7 +8,8 @@ from std_msgs.msg import Float64
 import json
 import serial
 
-ser = serial.Serial("/dev/ttyUSB0",115200)
+ser = serial.Serial("/dev/ttyUSB1",115200)
+j_mode = 1
 
 
 class MinimalSubscriber(Node):
@@ -31,6 +32,9 @@ class MinimalSubscriber(Node):
         else:
             getPos = int(2047 + (direcInput * radInput / 3.1415926 * 2048 * multiInput) + 0.5)
             return getPos
+        
+    # def angleGet(self, radInput, multiInput):
+    #     getAngle = int()
 
     def listener_callback(self, msg):
         a = msg.position
@@ -41,8 +45,25 @@ class MinimalSubscriber(Node):
         join4 = self.posGet(a[3],  1, 1)
         join5 = self.posGet(a[4], -1, 1)
         
-        data = json.dumps({'T':3,'P1':join1,'P2':join2,'P3':join3,'P4':join4,'P5':join5,'S1':0,'S2':0,'S3':0,'S4':0,'S5':0,'A1':60,'A2':60,'A3':60,'A4':60,'A5':60})
+            
+        #print the joint angle
+        print("--------------------")
+        print("joint1: ", a[0])
+        print("joint2: ", a[1])
+        print("joint3: ", a[2])
+        print("joint4: ", a[3])
+        print("joint5: ", a[4])
         
+        #for the st pose ctrl
+        # {"T":2,"P1":277.5104065,"P2":-13.75,"P3":276.5822754,"P4":90,"P5":180,"S1":10,"S5":200}
+        # {"T":2,"P1":277.5104065,"P2":-13.75,"P3":66.5822754,"P4":180,"P5":235,"S1":10,"S5":200}
+        # {"T":2,"P1":77.5104065,"P2":223.75,"P3":166.5822754,"P4":180,"P5":235,"S1":10,"S5":200}
+        if j_mode == 0:
+            data = json.dumps({'T':3,'P1':join1,'P2':join2,'P3':join3,'P4':join4,'P5':join5,'S1':0,'S2':0,'S3':0,'S4':0,'S5':0,'A1':60,'A2':60,'A3':60,'A4':60,'A5':60})
+        elif j_mode == 1:
+            data = json.dumps({'T':2,'P1':a[0],'P2':a[1],'P3':a[2],'P4':a[3],'P5':a[4],'S1':0,'S2':0,'S3':0,'S4':0,'S5':0,'A1':60,'A2':60,'A3':60,'A4':60,'A5':60})
+        # else:
+        #     data = json.dumps        
         ser.write(data.encode())
         
         print(data)
